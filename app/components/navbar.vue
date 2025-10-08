5851ba15d5f44cacd3f77e6ef1d1c8c0b7498166
joe <171643026+joe-238@users.noreply.github.com>
2025-10-07 23:14:16 -0400

    s

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -1,17 +1,20 @@
 <template>
   <nav class="w-[15%] h-screen flex flex-col pt-10 items-center bg-green-100">
-    <div class="flex flex-col gap-2">
-      <h2 class="font-bold text-xl cursor-pointer mb-4">SITHS KEY CLUB</h2>
+    <div class="flex flex-col gap-2 w-full">
+      <h2 class="font-bold text-xl cursor-pointer mb-4 self-center">
+        KEY CLUB
+      </h2>
       <NuxtLink
         v-for="link in navLinks.find((l) => l.category === category).links"
         :class="
           link.type === 'btn'
-            ? 'px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-200'
-            : 'px-4 py-2 text-blue-800 font-semibold rounded-lg hover:bg-blue-200 transition-colors duration-200'
+            ? ' ml-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-200'
+            : 'ml-4  p-2 w-full text-blue-800 font-semibold rounded-lg hover:bg-blue-200 transition-colors duration-200'
         "
         :key="link.name"
         :to="link.redirect"
-        >{{ link.name }}
+        ><i v-if="link.icon" class="fa-solid" :class="link.icon"></i>
+        {{ link.name }}
       </NuxtLink>
     </div>
   </nav>
@@ -23,26 +26,34 @@ const navLinks = [
   {
     category: "public",
     links: [
-      { redirect: "/", name: "Home" },
-      { redirect: "/about", name: "About" },
-      { redirect: "/contact", name: "Contact Us" },
-      { redirect: "/login", name: "Log In" },
+      { redirect: "/", name: "Home", icon: "fa-home" },
+      { redirect: "/about", name: "About", icon: "fa-address-card" },
+      { redirect: "/contact", name: "Contact Us", icon: "fa-phone" },
+      { redirect: "/login", name: "Log In", icon: "fa-right-to-bracket" },
       { redirect: "/signup", name: "Sign Up", type: "btn" },
     ],
   },
   {
     category: "student",
     links: [
-      { redirect: "/", name: "Home" },
-      { redirect: "/events", name: "Events" },
+      { redirect: "/", name: "Home", icon: "fa-home" },
+      { redirect: "/events", name: "Events", icon: "fa-calendar" },
     ],
   },
   {
     category: "admin",
     links: [
-      { redirect: "/", name: "Home" },
-      { redirect: "/eventManagement", name: "Event Management" },
-      { redirect: "/studentCharter", name: "Student Charter" },
+      { redirect: "/", name: "Home", icon: "fa-home" },
+      {
+        redirect: "/eventManagement",
+        name: "Event Management",
+        icon: "fa-calendar",
+      },
+      {
+        redirect: "/studentCharter",
+        name: "Student Charter",
+        icon: "fa-person",
+      },
     ],
   },
 ];
